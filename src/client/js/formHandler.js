7ff0743ced768fa18c940371e24f8fd79787501a
Mark Oribello <moribello@gmail.com>
2020-10-28 09:49:13 -0400

    feat: Update display to include user information

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,19 +5,15 @@ function handleSubmit(event) {
     let userLoc = document.getElementById('userLocText').value;
     let userDate = new Date(document.getElementById('userDateText').value);
     let todayDate = new Date();
-    let shortDate = friendlyDate(todayDate);
-    console.log(`User text date is ${userDate}.`)
-    console.log(`Today's date is ${shortDate}`)
+    let shortDate = friendlyDate(userDate);
+
     let countdown = Math.round((userDate-todayDate)/(1000*60*60*24));
-    // let countdown = userDate - shortDate;
-    console.log(userDate - shortDate);
-    alert(`You are going to ${userLoc} on ${userDate}, which is in ${countdown} days.`)
 
     function friendlyDate(dateObj) {
         var month = dateObj.getUTCMonth() + 1; //months from 1-12
         var day = dateObj.getUTCDate();
         var year = dateObj.getUTCFullYear();
-        var newdate = new Date(year + "/" + month + "/" + day + "/");
+        var newdate =  month + "/" + day + "/" + year;
         return newdate;
     }
 
@@ -51,7 +47,10 @@ function handleSubmit(event) {
 //         document.getElementById('agree').innerHTML = agrees;
 //         document.getElementById('conf').innerHTML = res.confidence;
 //         document.getElementById('irony').innerHTML = res.irony;
-            document.getElementById('dispCountdown').innerHTML = `<h1>You are leaving in ${countdown} days`;
+            document.getElementById('dispLoc').innerHTML = `<h1>${userLoc}</h1>`;
+            document.getElementById('dispCountdown').innerHTML = `<h1>You are leaving in ${countdown} days</h1>`;
+            document.getElementById('dispDate').innerHTML = `<h1>${friendlyDate(userDate)}</h1>`;
+
 //     })
 //}//bracket to close if / else statement
 }//bracket to close function
